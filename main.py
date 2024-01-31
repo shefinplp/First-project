401e24cf45a101c2521c4ecc2e609696147df172
shefinplp <156902653+shefinplp@users.noreply.github.com>
2024-01-31 20:31:05 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+
+from unittest import enterModuleContext
+
+
+year=int(input(enterModuleContext year)):
+if(year%4==0 and year%100!=0) or (year%400==0):
+    print("leap year")
+else :
+    print("not a leap year")
